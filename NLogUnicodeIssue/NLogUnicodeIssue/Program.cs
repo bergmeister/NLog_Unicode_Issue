1f4afe1cf03442fd2f8a8634ceccd3419a7e06b4
Christoph Bergmeister <christoph.bergmeister@willistowerswatson.com>
2018-01-22 10:30:37 +0000

    Repro of Nlog Unicode issue

---
+++ NLogUnicodeIssue/NLogUnicodeIssue/Program.cs
@@ -0,0 +1,13 @@
+﻿using NLog;
+
+namespace NLogUnicodeIssue
+{
+    internal class Program
+    {
+        internal static void Main()
+        {
+            Logger logger = LogManager.GetLogger("foo");
+            logger.Info("Japanese characters: 昨夜のコン");
+        }
+    }
+}
